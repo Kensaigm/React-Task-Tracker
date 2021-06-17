b0712273f82c1f1bc19c82831c7abfb2ce1a75bd
Kensaigm <Kensaigm@users.noreply.github.com>
2021-06-17 13:46:03 -0700

    Update App.js
    Moved mock data to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import Header from './components/Header'
-import Tasks from './components/Tasks';
+import Tasks from './components/Tasks'
+import { useState } from 'react';
 
 function App() {
+
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1,
+        text: 'Check in Updates',
+        day: 'June 25th at 5:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Code Review',
+        day: 'June 28th at 8:00am',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Update Jira',
+        day: 'Everyday at 4:30pm',
+        remidner: false,
+      }
+    ]
+  )
+
   return (
     <div className="container">
       <Header />
