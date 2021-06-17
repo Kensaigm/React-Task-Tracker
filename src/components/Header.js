6172d2f93fa53355dffe51ebf089d67d8f4c1561
Kensaigm <Kensaigm@users.noreply.github.com>
2021-06-17 13:00:20 -0700

    Update Header.js
    Added event for Button

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,13 @@ import PropTypes from 'prop-types';
 import Button from './Button'
 
 const Header = (props) => {
+    const onClick = () => {
+
+    }
     return (
         <header className='header'>
             <h1 style={headingStyle}>{props.title}</h1>
-            <Button color='green' text='Add' />
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
