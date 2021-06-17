81d6429ce786ffabc1d8138841af651d7225e3c7
Kensaigm <Kensaigm@users.noreply.github.com>
2021-06-17 12:43:32 -0700

    Added Button.js
    Moved Button from Header to Button.js and added support for input.

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+const Button = (props) => {
+    return (
+        <>
+            <button className='btn'>Add</button>
+        </>
+    )
+}
+
+export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
-
+import Button from './Button'
 
 const Header = (props) => {
     return (
         <header className='header'>
             <h1 style={headingStyle}>{props.title}</h1>
-            <button className='btn'>Add</button>
+            <Button color='green' text='Add' />
         </header>
     )
 }
