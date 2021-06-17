88582cbadc93dfe0c18936268a4e64b43aa347e1
Kensaigm <Kensaigm@users.noreply.github.com>
2021-06-17 12:09:21 -0700

    Modified Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="container">
-      <h1>Task Tracker Home</h1>
-      <h2>Tasks</h2>
+      <Header />
     </div>
   );
 }
