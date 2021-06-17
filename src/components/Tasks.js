50167f647b2d1210f791d88b6be3fbbd42acc9fa
Kensaigm <Kensaigm@users.noreply.github.com>
2021-06-17 13:28:21 -0700

    Update Tasks.js
    Added sample/mock data for testing.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,4 +1,23 @@
-import React from 'react'
+const tasks = [
+    {
+        id: 1,
+        text: 'Check in Updates',
+        day: 'June 25th at 5:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Code Review',
+        day: 'June 28th at 8:00am',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Update Jira',
+        day: 'Everyday at 4:30pm',
+        remidner: false,
+    }
+]
 
 const Tasks = () => {
     return (
