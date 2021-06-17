e6bc767d080e2128841becdd22543a9f08f6e843
Kensaigm <Kensaigm@users.noreply.github.com>
2021-06-17 12:56:53 -0700

    Update Button.js
    Log button clicked for testing

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,26 @@
+import PropTypes from 'prop-types'
+
 const Button = ({ color, text }) => {
+    const onClick = () => {
+        console.log('Button Clicked')
+    }
     return (
         <>
-            <button style={{ backgroundColor: color }} className='btn'>{text}</button>
+            <button
+                onClick={onClick}
+                style={{ backgroundColor: color }} className='btn'>{text}
+            </button>
         </>
     )
+}
+
+Button.defaultProps = {
+    color: 'steelblue',
+}
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string,
 }
 
 export default Button
