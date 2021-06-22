388021da4d26bfd1a6976dccba1fe0d8a061b4d7
Kensaigm <Kensaigm@users.noreply.github.com>
2021-06-22 12:01:28 -0700

    Updated for Save Task to work
    removed isRequired from text as we now have a validator.  addTask now assigns an ID for each new task.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks'
 import Add from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
 
   const [tasks, setTasks] = useState(
     [
@@ -29,7 +30,10 @@ function App() {
   )
 
   const addTask = (task) => {
-    console.log(task)
+    var sorted_keys = Object.keys(tasks).sort(function (a, b) { return tasks[b] - tasks[a]; });
+    const id = sorted_keys.length + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
   }
 
   const deleteTask = (id) => {
@@ -46,9 +50,9 @@ function App() {
     <div className="container">
       <Header />
       <Add onAdd={addTask} />
-      {tasks.length > 0 ? <Tasks tasks={tasks}
+      {tasks.length > 0 ? (<Tasks tasks={tasks}
         onDelete={deleteTask}
-        onToggle={toggleReminder} /> : (<h3>No Tasks are scheduled at this time.</h3>)}
+        onToggle={toggleReminder} />) : (<h3>No Tasks are scheduled at this time.</h3>)}
     </div>
   );
 }
