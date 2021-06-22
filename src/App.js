9fba6864df23020dc6a1b6b89b222ff08775130f
Kensaigm <Kensaigm@users.noreply.github.com>
2021-06-22 10:21:07 -0700

    Modified addTask and AddTask
    Event handler, verification, onSubmit, and reset form.

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ function App() {
     ]
   )
 
-  const AddTask = () => {
-
+  const addTask = (task) => {
+    console.log(task)
   }
 
   const deleteTask = (id) => {
@@ -45,7 +45,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <Add />
+      <Add onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks}
         onDelete={deleteTask}
         onToggle={toggleReminder} /> : (<h3>No Tasks are scheduled at this time.</h3>)}
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 
-function AddTask() {
+const AddTask = ({ onAdd }) => {
     const [text, setText] = useState('')
     const [day, setDay] = useState('')
     const [reminder, setReminder] = useState(false)
 
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if (!text) {
+            alert('Please add a task!')
+            return
+        }
+
+        onAdd({ text, day, reminder })
+
+        setText('')
+        setDay('')
+        setReminder(false)
+    }
+
     return (
-        <form className='add-form'>
+        <form className='add-form' onSubmit={onSubmit}>
             <div className='form-control'>
                 <label>Task</label>
                 <input type='text' placeholder='Add Task'
@@ -20,7 +35,7 @@ function AddTask() {
             <div className='form-control form-control-check'>
                 <label>Set Reminder</label>
                 <input type='checkbox'
-                    value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} />
+                    value={reminder} checked={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} />
             </div>
 
             <input type='submit' value='Save Task' className='btn btn-block' />
