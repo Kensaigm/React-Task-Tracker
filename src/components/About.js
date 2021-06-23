6ffaf07bf401bbeebab30c8a8c3b263c4e848bcd
Kensaigm <Kensaigm@users.noreply.github.com>
2021-06-22 17:02:31 -0700

    Update About.js
    Updated About.js

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <h4>Version 1.0.0</h4>
+            <a href='https://github.com/Kensaigm/React-Task-Tracker/'>GitHub Link</a>
+            <a href='/'>Go Back</a>
+        </div>
+    )
+}
+
+export default About
