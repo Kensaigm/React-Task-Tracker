f558a9f14584775f8ea0f03fbda42d2a3bff6e2b
Kensaigm <Kensaigm@users.noreply.github.com>
2021-06-22 17:29:27 -0700

    Update About.js
    Removed A links in favor of using Route Link

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom'
+
 const About = () => {
     return (
         <div>
             <h4>Version 1.0.0</h4>
-            <p><a href='https://github.com/Kensaigm/React-Task-Tracker/'>GitHub Link</a></p>
-            <p><a href='/'>Go Back</a></p>
+            <p><Link to='https://github.com/Kensaigm/React-Task-Tracker/'>GitHub Link</Link></p>
+            <p><Link to='/'>Go Back</Link></p>
         </div>
     )
 }
